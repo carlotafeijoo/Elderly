e163581361263c5cc65c7f5973ddad190a0f7a5e
carlotafeijoo <carlota.feijoorodriguez@usp.ceu.es>
2023-10-22 16:17:11 +0200

    pruebas con sockets y primeros hechos

--- src/ui/ElderlyMenuResidencialArea.java
+++ src/ui/ElderlyMenuResidencialArea.java
@@ -1,8 +1,7 @@
 package ui;
 
 import java.io.*;
-
-
+import java.net.Socket;
 import java.security.MessageDigest;
 
 import java.text.SimpleDateFormat;
@@ -14,30 +13,47 @@ import exceptions.InputException;
 import POJOS.*;
 
 public class ElderlyMenuResidencialArea {
-
+	static OutputStream os = null;
+	static PrintWriter pw = null;
+	
+	static BufferedReader br = null;
+	static Socket so = null;
 	private static BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
 
-	//private static ElderlyManager elderlyManager;
 
-	//private static DoctorManager DoctorManager;
-
-	//private static UserManager userManager;
-
-	//private static TaskManager tasksManager;
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
 		System.out.println("WELCOME TO THE RESIDENCIAL AREA DATA BASE");
-		/*
-		JDBCManager jdbcManager = new JDBCManager();
+		
+		Socket so = new Socket("localhost", 9009);
+		// el cliente lee lineas pero tambien manda
+		br = new BufferedReader(new InputStreamReader(so.getInputStream()));
+		os = so.getOutputStream();
+		pw = new PrintWriter(os, true);
 
-		// initialize database JDBC
-		elderlyManager = new JDBCElderlyManager(jdbcManager);
-		DoctorManager = new JDBCDoctorManager(jdbcManager);
-		tasksManager = new JDBCTasksManager(jdbcManager);
-		// initialize database JPA
-		userManager = new JPAUserManager();*/
 		mainMenu();
 
+	}
+	
+	private static void releaseResources(PrintWriter printWriter, BufferedReader br, OutputStream outputStream,
+			Socket so2) {
+		printWriter.close();
+		try {
+			outputStream.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		try {
+			br.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		try {
+			so2.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public static void mainMenu() {
@@ -56,10 +72,10 @@ public class ElderlyMenuResidencialArea {
 					loginElderly();
 					break;
 
-
-
 				case 2:
 					System.out.println("YOU HAVE EXIT THE RESIDENCIAL AREA DATA BASE");
+					releaseResources(pw, br, os, so);
+
 					System.exit(3);
 					break;
 
@@ -73,124 +89,7 @@ public class ElderlyMenuResidencialArea {
 		}
 
 	}
-
-
-	private static void elderlyMenu(int User_id) {
-
-		try {
-			int choice;
-			do {
-
-				System.out.println("1. Record signal.  ");
-				System.out.println("2. See my tasks");
-				System.out.println("3. Back");
-
-				choice = InputException.getInt("Introduce your choice: ");
-
-				switch (choice) {
-
-				case 1:
-					//AQUI: FUNCION DE LLAMAR AL BITALINO
-					break;
-				case 2:
-					int elderly_id= 0;//elderlyManager.searchElderlyIdfromUId(User_id);
-                 List <Task> tasks=null;//elderlyManager.seeTasks(elderly_id);
-                 for (int i=0;i<tasks.size();i++) {
-                	 System.out.println(tasks.get(i).toStringtoElderly());
-                 }
-					break;				
-					
-				case 3:
-						mainMenu();
-						break;
-
-				default:
-					break;
-				}
-			} while (true);
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	public static void registerElderly() throws Exception {
-
-		System.out.println("Input the information of the new elderly: ");
-
-		String name = InputException.getString(" Name: ");
-
-		int day = InputException.getInt("Day of birth:  ");
-		int month = InputException.getInt("Month of birth:  ");
-		int year = InputException.getInt("Year of birth:  ");
-		int dni = InputException.getInt("DNI without letter:  ");
-
-		ArrayList<Doctor> doctores = null; //DoctorManager.searchAllDoctors();
-
-		for (int i = 0; i < doctores.size(); i++) {
-			System.out.println(doctores.get(i).toStringForPatients());
-		}
-		int doctor_id = InputException.getInt("put the id of your doctor:  ");
-
-		System.out.println(" info: your username will be your dni ");
-		Date dob = new Date(year, month, day);
-
-		String username = "" + dni;
-		String password = InputException.getString("Password: ");
-		MessageDigest md = MessageDigest.getInstance("MD5");
-		md.update(password.getBytes());
-		byte[] digest = md.digest();
-
-		User u = new User(username, digest);
-		Role role = null;//userManager.getRole("Elderly");
-		u.setRole(role);
-		role.addUser(u);
-		//userManager.newUser(u);
-
-		Elderly elderly = new Elderly(name, dni, dob, doctor_id);
-
-		//elderlyManager.addElderly(elderly);
-		elderlyMenu(u.getId());
-
-	}
-
-
-
-	public static void logIn() throws Exception {
-
-		System.out.println("Username or dni without letter:");
-		String username = read.readLine();
-		String password = InputException.getString("Password: ");
-		MessageDigest md = MessageDigest.getInstance("MD5");
-		md.update(password.getBytes());
-		byte[] digest = md.digest();
-
-		User u = null;//userManager.checkPassword(username, digest);
-
-		if (u == null) {
-			System.out.println("User not found");
-			mainMenu();
-		}
-
-
-
-		if (u != null && u.getRole().getName().equals("Elderly")) {
-			Integer id = u.getId();
-
-			int elderly_id = 0;//elderlyManager.searchElderlyIdfromUId(id);
-
-			Elderly elderly =null;// elderlyManager.searchElderlyById(elderly_id);
-
-			System.out.println(elderly);
-			System.out.println("Login successful!");
-			elderlyMenu(u.getId());
-
-		}
-
-	}
-
-
-
+	
 	private static void loginElderly() throws Exception {
 		System.out.println("1. Register");
 		System.out.println("2. Log in ");
@@ -201,6 +100,7 @@ public class ElderlyMenuResidencialArea {
 		case 1:
 			// Call method REGISTER
 			registerElderly();
+			loginElderly();
 			break;
 
 		case 2:
@@ -217,7 +117,134 @@ public class ElderlyMenuResidencialArea {
 			break;
 		}
 	}
+	
+	public static void registerElderly() throws Exception {
 
+		System.out.println("Input the information of the new elderly: ");
 
+		String name = InputException.getString("Name: ");
 
+		System.out.println("Enter the year of birth:");
+		int year = Integer.parseInt(read.readLine());
+
+		System.out.println("Enter the month of birth:");
+		int month = Integer.parseInt(read.readLine());
+
+		System.out.println("Enter the day of birth:");
+		int day = Integer.parseInt(read.readLine());
+
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		String dobStr = String.format("%04d-%02d-%02d", year, month, day);
+		java.util.Date utilDate = dateFormat.parse(dobStr);
+		java.sql.Date dob = new java.sql.Date(utilDate.getTime());
+
+		System.out.println("Your dni will be used as your username");
+		System.out.println("DNI without letter:  ");
+		int dni = Integer.parseInt(read.readLine());
+
+		/*ArrayList<Doctor> doctores = null; // DoctorManager.searchAllDoctors();
+
+		for (int i = 0; i < doctores.size(); i++) {
+			System.out.println(doctores.get(i).toStringForPatients());
+		}*/
+		
+		Integer doctor_id = InputException.getInt("Put the id of your doctor: ");
+		String username = "" + dni;
+		
+		/*MessageDigest md = MessageDigest.getInstance("MD5");
+		md.update(password.getBytes());
+		byte[] digest = md.digest();
+		User u = new User(username, digest);
+		Role role = null;// userManager.getRole("Elderly");
+		u.setRole(role);
+		role.addUser(u);
+		// userManager.newUser(u);*/
+
+		Elderly elderly = new Elderly(name, dni, dob, doctor_id);
+		
+		String password = InputException.getString("Password: ");
+		
+		pw.println("addElderly");
+		pw.println(username);
+		pw.println(password);
+		pw.println(elderly.toString());
+
+		br.readLine();
+
+		// elderlyManager.addElderly(elderly);
+		//elderlyMenu(u.getId());
+
+	}
+	
+	public static void logIn() throws Exception {
+
+		System.out.println("Username or dni without letter:");
+		String username = read.readLine();
+		String password = InputException.getString("Password: ");
+		MessageDigest md = MessageDigest.getInstance("MD5");
+		md.update(password.getBytes());
+		byte[] digest = md.digest();
+
+		User u = null;// userManager.checkPassword(username, digest);
+
+		if (u == null) {
+			System.out.println("User not found");
+			mainMenu();
+		}
+
+		if (u != null && u.getRole().getName().equals("Elderly")) {
+			Integer id = u.getId();
+
+			int elderly_id = 0;// elderlyManager.searchElderlyIdfromUId(id);
+
+			Elderly elderly = null;// elderlyManager.searchElderlyById(elderly_id);
+
+			System.out.println(elderly);
+			System.out.println("Login successful!");
+			elderlyMenu(u.getId());
+
+		}
+
+	}
+
+	private static void elderlyMenu(int User_id) {
+
+		try {
+			int choice;
+			do {
+
+				System.out.println("1. Record signal.  ");
+				System.out.println("2. See my tasks");
+				System.out.println("3. Back");
+
+				choice = InputException.getInt("Introduce your choice: ");
+
+				switch (choice) {
+
+				case 1:
+					// AQUI: FUNCION DE LLAMAR AL BITALINO
+					break;
+				case 2:
+					int elderly_id = 0;// elderlyManager.searchElderlyIdfromUId(User_id);
+					List<Task> tasks = null;// elderlyManager.seeTasks(elderly_id);
+					for (int i = 0; i < tasks.size(); i++) {
+						System.out.println(tasks.get(i).toStringtoElderly());
+					}
+					break;
+
+				case 3:
+					mainMenu();
+					break;
+
+				default:
+					break;
+				}
+			} while (true);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	
 }
